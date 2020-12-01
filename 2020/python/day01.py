b5db09b6c34aad6e505cae5c4f162093e94415bf
trox667 <trox667@googlemail.com>
2020-12-01 06:43:22 +0100

    day01 - part1&2 refactor

--- 2020/python/day01.py
+++ 2020/python/day01.py
@@ -1,71 +1,46 @@
 import unittest
+import itertools
+import functools
 
 
-def is_2020(a, b):
-    if min(a, b) == 0:
+def is_2020(combinations):
+    if 0 in combinations:
         False
-    if a + b == 2020:
+
+    if functools.reduce(lambda x, y: x+y, combinations) == 2020:
         return True
     else:
         return False
 
 
-def run(items):
-    for i in range(0, len(items)):
-        for j in range(0, len(items)):
-            if i == j:
-                continue
-            a = items[i]
-            b = items[j]
-            if is_2020(a, b):
-                return a * b
+def run(items, size=2, compare_func=is_2020):
+    combinations = itertools.combinations(items, size)
+    for combination in combinations:
+        if compare_func(combination):
+            return functools.reduce(lambda x, y: x*y, combination)
+    return 0
 
 
 def part1():
-    with open('../inputs/input01') as input:
-        print(run([int(item.strip()) for item in input if not item.isspace()]))
-
-
-def is_2020_3(a, b, c):
-    if min(a, b, c) == 0:
-        False
-    if a + b + c == 2020:
-        return True
-    else:
-        return False
-
-
-def run2(items):
-    for i in range(0, len(items)):
-        for j in range(0, len(items)):
-            for h in range(0, len(items)):
-                if i == j or i == h or j == h:
-                    continue
-                a = items[i]
-                b = items[j]
-                c = items[h]
-                if is_2020_3(a, b, c):
-                    return a * b * c
+    print(run(read_input()))
 
 
 def part2():
-    with open('../inputs/input01') as input:
-        print(run2([int(item.strip())
-                    for item in input if not item.isspace()]))
+    print(run(read_input(), 3))
 
 
 def read_input():
-    with open('input') as input:
-        pass
-    pass
+    with open('../inputs/input01') as input:
+        return [int(item.strip()) for item in input if not item.isspace()]
+    return []
 
 
 class TestPart1(unittest.TestCase):
     def test_is_2020(self):
-        self.assertEqual(is_2020(1721, 299), True)
-        self.assertEqual(is_2020(0, 0), False)
-        self.assertEqual(is_2020(-100, -1900), False)
-        self.assertEqual(is_2020(100, 1900), False)
+        self.assertEqual(is_2020((1721, 299)), True)
+        self.assertEqual(is_2020((0, 0)), False)
+        self.assertEqual(is_2020((-100, -1900)), False)
+        self.assertEqual(is_2020((100, 1900)), False)
 
     def test_run(self):
         self.assertEqual(run([1721, 299]), 514579)
@@ -74,12 +49,12 @@ class TestPart1(unittest.TestCase):
 
 class TestPart2(unittest.TestCase):
     def test_is_2020_3(self):
-        self.assertEqual(is_2020_3(979, 366, 675), True)
-        self.assertEqual(is_2020_3(0, 1721, 299), True)
+        self.assertEqual(is_2020((979, 366, 675)), True)
+        self.assertEqual(is_2020((0, 1721, 299)), True)
 
     def test_run2(self):
-        self.assertEqual(run2([979, 366, 675]), 241861950)
-        self.assertEqual(run2([0, 1, 979, 366, 675, -1, 3]), 241861950)
+        self.assertEqual(run([979, 366, 675], 3), 241861950)
+        self.assertEqual(run([0, 1, 979, 366, 675, -1, 3], 3), 241861950)
 
 
 if __name__ == '__main__':
