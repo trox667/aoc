0544c58fa22eab42d62bdbe46f2692dce253da4d
trox667 <trox667@googlemail.com>
2020-12-10 20:35:31 +0100

    day10 - part1&2

--- 2020/python/day10.py
+++ 2020/python/day10.py
@@ -1,54 +1,55 @@
 import unittest
-import itertools
-
-
-def next_in_range(joltages, joltage, range=3):
-    r = list()
-    for j in joltages:
-        if j <= joltage+3 or j-1 <= joltage+3 or j-2 <= joltage+3 or j-3 <= joltage+3:
-            r.append(j)
-    if len(r) == 0:
-        return (None, None, None)
-    joltages.remove(min(r))
-    return (joltages, min(r), min(r)-joltage)
-
-
-def max_jolt(joltages):
-    return max(joltages) + 3
+import collections
 
 
 def run(joltages):
-    one_diff = 0
-    three_diff = 0
-    curr = 0
-    m = max_jolt(joltages)
-    while True:
-        joltages, curr, r = next_in_range(joltages, curr)
-        if not joltages and not curr and not r:
-            return 0
-        if r == 1:
-            one_diff += 1
-        elif r == 3:
-            three_diff += 1
-        if curr+3 >= m:
-            three_diff += 1
-            if one_diff == 0:
-                return three_diff
-            if three_diff == 0:
-                return one_diff
-            return one_diff * three_diff
+    joltages.insert(0, 0)
+    joltages.append(max(joltages)+3)
+    joltages.sort()
+    diff_one = 0
+    diff_three = 0
+    for i in range(0, len(joltages)-1):
+        diff = joltages[i+1] - joltages[i]
+        if diff == 3:
+            diff_three += 1
+        elif diff == 1:
+            diff_one += 1
+    return diff_one * diff_three
 
 
 def part1():
     print(run(read_input()))
 
+#     0
+#     1
+#     4
+#     5
+#     6 -> 5 | 4
+#     7 -> 6 (5|4) | 5 | 4
+#    10 -> 7 -> 6 (5|4) | 5 | 4
+#    11 -> 7 -> 6 (5|4) | 5 | 4
+#    12 -> 11 | 10 ...
+
 
 def run2(joltages):
-    pass
+    joltages.insert(0, 0)
+    joltages.sort()
+
+    all_options = collections.defaultdict(int)
+    all_options[0] = 1
+
+    for i in range(1, len(joltages)):
+        jolt = joltages[i]
+        pv1 = all_options[jolt-1]
+        pv2 = all_options[jolt-2]
+        pv3 = all_options[jolt-3]
+        all_options[jolt] = pv1 + pv2 + pv3
+
+    return all_options[joltages[-1]]
 
 
 def part2():
-    pass
+    print(run2(read_input()))
 
 
 def read_input():
@@ -102,23 +103,6 @@ test_input2 = """28
 
 
 class TestPart1(unittest.TestCase):
-    def test_next_in_range(self):
-        joltages = [int(l) for l in test_input.split('\n')]
-        joltages, d, r = next_in_range(joltages, 0)
-        self.assertEqual(d, 1)
-        self.assertEqual(r, 1)
-        joltages, d, r = next_in_range(joltages, 1)
-        self.assertEqual(d, 4)
-        self.assertEqual(r, 3)
-        joltages, d, r = next_in_range(joltages, 4)
-        self.assertEqual(d, 5)
-        self.assertEqual(r, 1)
-        joltages, d, r = next_in_range(joltages, 5)
-        joltages, d, r = next_in_range(joltages, 6)
-        joltages, d, r = next_in_range(joltages, 7)
-        joltages, d, r = next_in_range(joltages, 10)
-        self.assertEqual(d, 11)
-        self.assertEqual(r, 1)
 
     def test_run(self):
         joltages = [int(l) for l in test_input.split('\n')]
@@ -129,12 +113,16 @@ class TestPart1(unittest.TestCase):
 
 
 class TestPart2(unittest.TestCase):
+
     def test_run2(self):
         joltages = [int(l) for l in test_input.split('\n')]
         self.assertEqual(run2(joltages), 8)
 
+        joltages = [int(l) for l in test_input2.split('\n')]
+        self.assertEqual(run2(joltages), 19208)
+
 
 if __name__ == '__main__':
-    # part1()
+    part1()
+    part2()
     unittest.main()
-    pass
