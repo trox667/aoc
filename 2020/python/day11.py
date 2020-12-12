584c09cb4c79417684a74acc4a5515f7d4a0c54a
trox667 <trox667@googlemail.com>
2020-12-12 17:41:50 +0100

    day11 - part1&2

--- 2020/python/day11.py
+++ 2020/python/day11.py
@@ -11,15 +11,14 @@ class Grid:
     def add(self, position, value):
         self.data[position] = value
 
-
-def create_grid(width, height):
-    grid = Grid()
-    grid.width = width
-    grid.height = height
-    for y in range(0, height):
-        for x in range(0, width):
-            grid.add((x, y), '.')
-    return grid
+    def clone_empty(self):
+        grid = Grid()
+        grid.width = self.width
+        grid.height = self.height
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                grid.add((x, y), '.')
+        return grid
 
 
 def parse(lines):
@@ -32,65 +31,7 @@ def parse(lines):
     return grid
 
 
-def draw(grid):
-    for y in range(0, grid.height):
-        line = ''
-        for x in range(0, grid.width):
-            line += grid.data[(x, y)]
-        print(line)
-
-
-def neighbors(grid, position):
-    count = 0
-    (px, py) = position
-    for y in range(-1, 2):
-        for x in range(-1, 2):
-            if x == 0 and y == 0:
-                continue
-            if not (px + x, py + y) in grid.data:
-                continue
-            if grid.data[(px + x, py + y)] == '#':
-                count += 1
-    return count
-
-
-def rules(grid, new_grid, position):
-    nc = neighbors(grid, position)
-    seat = grid.data[position]
-    if seat == 'L' and nc == 0:
-        new_grid.add(position, '#')
-    elif seat == '#' and nc >= 4:
-        new_grid.add(position, 'L')
-    else:
-        new_grid.add(position, seat)
-
-
-def turn(grid):
-    new_grid = create_grid(grid.width, grid.height)
-    for y in range(0, grid.height):
-        for x in range(0, grid.width):
-            rules(grid, new_grid, (x, y))
-    return new_grid
-
-
-def count_seats(grid):
-    return sum(seat == '#' for seat in grid.data.values())
-
-
-def run(input):
-    grid = parse(input)
-    while True:
-        new_grid = turn(grid)
-        if new_grid.data == grid.data:
-            return count_seats(new_grid)
-        grid = new_grid
-
-
-def part1():
-    print(run(read_input()))
-
-
-def in_sight(grid, position):
+def adjacent_seats(grid, position, infinite=True):
     count = 0
     dirs = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1),
             (1, -1)]
@@ -106,41 +47,57 @@ def in_sight(grid, position):
                 break
             elif grid.data[check_pos] == 'L':
                 break
-            scale += 1
-            check_pos = (px + x * scale, py + y * scale)
+            if infinite:
+                scale += 1
+                check_pos = (px + x * scale, py + y * scale)
+            else:
+                break
     return count
 
 
-def rules2(grid, new_grid, position):
-    nc = in_sight(grid, position)
+def neighbors(grid, position):
+    return adjacent_seats(grid, position, False)
+
+
+def in_sight(grid, position):
+    return adjacent_seats(grid, position, True)
+
+
+def turn(grid, neighbor_func, max_adjacent_seats):
+    new_grid = grid.clone_empty()
+    for y in range(0, grid.height):
+        for x in range(0, grid.width):
+            rules(grid, new_grid, (x, y), neighbor_func, max_adjacent_seats)
+    return new_grid
+
+
+def rules(grid, new_grid, position, neighbor_func, max_adjacent_seats):
+    nc = neighbor_func(grid, position)
     seat = grid.data[position]
     if seat == 'L' and nc == 0:
         new_grid.add(position, '#')
-    elif seat == '#' and nc >= 5:
+    elif seat == '#' and nc >= max_adjacent_seats:
         new_grid.add(position, 'L')
     else:
         new_grid.add(position, seat)
 
 
-def turn2(grid):
-    new_grid = create_grid(grid.width, grid.height)
-    for y in range(0, grid.height):
-        for x in range(0, grid.width):
-            rules2(grid, new_grid, (x, y))
-    return new_grid
-
-
-def run2(input):
+def run(input, neighbor_func=neighbors, max_adjacent_seats=4):
     grid = parse(input)
     while True:
-        new_grid = turn2(grid)
+        new_grid = turn(grid, neighbor_func, max_adjacent_seats)
         if new_grid.data == grid.data:
-            return count_seats(new_grid)
+            return sum(seat == '#' for seat in new_grid.data.values())
+
         grid = new_grid
 
 
+def part1():
+    print(run(read_input()))
+
+
 def part2():
-    print(run2(read_input()))
+    print(run(read_input(), in_sight, 5))
 
 
 def read_input():
@@ -208,11 +165,11 @@ class TestPart2(unittest.TestCase):
         self.assertEqual(in_sight(grid, (3, 3)), 0)
 
     def test_run2(self):
-        self.assertEqual(run2(test_input.split('\n')), 26)
+        self.assertEqual(run(test_input.split('\n'), in_sight, 5), 26)
 
 
 if __name__ == '__main__':
-    # part1()
+    part1()
     part2()
     unittest.main()
     pass
