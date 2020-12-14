b91d5456e35855080d727c97543e45e81459d930
trox667 <trox667@googlemail.com>
2020-12-14 22:37:13 +0100

    day14 - part1&2 meh

--- 2020/python/day14.py
+++ 2020/python/day14.py
@@ -1,10 +1,22 @@
 import unittest
 import re
+import itertools
+
+
+def replacenth(string, sub, wanted, n):
+    where = [m.start() for m in re.finditer(sub, string)][n-1]
+    before = string[:where]
+    after = string[where:]
+    after = after.replace(sub, wanted, 1)
+    newString = before + after
+    return newString
+
 
 pattern = r"\[(?P<index>\d+)\] = (?P<value>\d+)"
 
 
 def parse(line: str):
+    line = line.strip()
     if line.startswith('mask'):
         _, value = line.split(' = ')
         bit_and = value[value.index('1'):].replace('X', '1')
@@ -38,12 +50,77 @@ def part1():
     print(run(instructions))
 
 
-def run2():
-    pass
+def prepend(i):
+    s = bin(i)[2:]
+    t = ''
+    for i in range(0, 36-len(s)):
+        t += '0'
+
+    t += s
+    return t
+
+
+def create_combination(i, s):
+    # assemble the result string value + mask
+    p = list(prepend(i))
+    assert len(p) == len(s)
+    for i in range(0, len(p)):
+        b = s[i]
+        if b == '1':
+            p[i] = '1'
+        elif b == 'X':
+            p[i] = 'X'
+    s = ''.join(p)
+    S = set()
+    xs = s.count('X')
+    perms = force(xs)
+
+    # replace all X with permutations
+    S = set()
+    for p in perms:
+        t = s
+        P = list(p)
+        for i in range(0, len(P)):
+            t = replacenth(t, 'X', P[i], 1)
+        S.add(t)
+
+    return [int(s, 2) for s in S]
+
+
+def force(l):
+    return [seq for seq in itertools.product("01", repeat=l)]
+
+
+def parse2(line: str):
+    line = line.strip()
+    if line.startswith('mask'):
+        _, value = line.split(' = ')
+        return (value, None, None)
+    else:
+        matches = re.search(pattern, line)
+        index = int(matches.group('index'))
+        value = int(matches.group('value'))
+        return (None, index, value)
+
+
+def run2(instructions):
+    mem = dict()
+    mask = ''
+    for instruction in instructions:
+        if instruction[0] != None:
+            mask = instruction[0]
+        else:
+            index = instruction[1]
+            value = instruction[2]
+            for c in create_combination(index, mask):
+                mem[c] = value
+
+    return sum(mem.values())
 
 
 def part2():
-    pass
+    instructions = [parse2(line) for line in read_input()]
+    print(run2(instructions))
 
 
 def read_input():
@@ -55,6 +132,11 @@ test_input = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0"""
+
+test_input2 = """mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"""
 
 
 class TestPart1(unittest.TestCase):
@@ -78,9 +160,12 @@ class TestPart1(unittest.TestCase):
 
 class TestPart2(unittest.TestCase):
     def test_run2(self):
-        pass
+        instructions = [parse2(line) for line in test_input2.split(
+            '\n') if not line.isspace()]
+        self.assertEqual(run2(instructions), 208)
 
 
 if __name__ == '__main__':
     part1()
+    part2()
     unittest.main()
