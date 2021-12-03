dedbe492f6409f354ee12bd277f398a2e01507e4
trox667 <trox667@googlemail.com>
2021-12-03 20:04:49 +0100

    day3

--- 2021/cpp/day_3.cpp
+++ 2021/cpp/day_3.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cassert>
 #include <cmath>
+#include <tuple>
 #include "day_3.hpp"
 
 namespace day3 {
@@ -24,7 +25,7 @@ namespace day3 {
             "01010"
     };
 
-    std::vector<int>
+    [[nodiscard]] std::vector<int>
     binary_number_from_lines(const std::vector<std::string> &lines) {
         std::vector<int> result(lines.size());
         std::transform(lines.cbegin(), lines.cend(), result.begin(),
@@ -35,11 +36,11 @@ namespace day3 {
     }
 
 
-    int pow2(size_t power) {
+    [[nodiscard]] int pow2(size_t power) {
         return static_cast<int>(pow(2, static_cast<double>(power)));
     }
 
-    int create_mask(size_t length) {
+    [[nodiscard]] int create_mask(size_t length) {
         int mask = 0;
         for (auto i = 0; i < length; ++i) {
             mask += pow2(length - 1 - i);
@@ -77,7 +78,64 @@ namespace day3 {
         return gamma * (gamma ^ mask);
     }
 
+    [[nodiscard]] bool is_common_one(int index,
+                                     const std::vector<int> &binary_numbers) {
+        auto half = static_cast<size_t>(ceil(
+                static_cast<double>(binary_numbers.size()) / 2.0));
+        auto ones = 0;
+
+        for (auto binary_number: binary_numbers) {
+            if (binary_number >> index & 1) {
+                ones++;
+            }
+            if (ones > half || ones == half) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    std::vector<int>
+    filter(int index, int value, const std::vector<int> &binary_numbers) {
+        std::vector<int> result(binary_numbers.size());
+        auto it = std::copy_if(binary_numbers.cbegin(), binary_numbers.cend(),
+                               result.begin(),
+                               [index, value](int binary_number) {
+                                   return (binary_number >> index & 1) == value;
+                               });
+        result.resize(std::distance(result.begin(), it));
+        return result;
+    }
+
     [[nodiscard]] int part2(const std::vector<std::string> &lines) {
-        return 0;
+        auto binary_length = static_cast<int>(lines[0].size());
+        auto binary_numbers = binary_number_from_lines(lines);
+
+        auto oxygen_generator_rating = binary_numbers;
+        auto co2_scrubber_rating = binary_numbers;
+
+        for (int i = binary_length - 1; i >= 0; --i) {
+            if (oxygen_generator_rating.size() > 1) {
+                if (is_common_one(i, oxygen_generator_rating)) {
+                    oxygen_generator_rating = filter(i, 1,
+                                                     oxygen_generator_rating);
+                } else {
+                    oxygen_generator_rating = filter(i, 0,
+                                                     oxygen_generator_rating);
+                }
+            }
+
+            if (co2_scrubber_rating.size() > 1) {
+                if (is_common_one(i, co2_scrubber_rating)) {
+                    co2_scrubber_rating = filter(i, 0, co2_scrubber_rating);
+                } else {
+                    co2_scrubber_rating = filter(i, 1, co2_scrubber_rating);
+                }
+            }
+        }
+        assert(oxygen_generator_rating.size() == 1 &&
+               co2_scrubber_rating.size() == 1);
+        return oxygen_generator_rating[0] * co2_scrubber_rating[0];
     }
 }
