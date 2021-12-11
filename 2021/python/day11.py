fd7e8cd0bd2e70b656ddabd26416707d643f3a83
trox667 <trox667@googlemail.com>
2021-12-11 12:32:54 +0100

    day11

--- 2021/python/day11.py
+++ 2021/python/day11.py
@@ -1,21 +1,3 @@
-small_test = """11111
-19991
-19191
-19991
-11111"""
-
-test = """5483143223
-2745854711
-5264556173
-6141336146
-6357385478
-4167524645
-2176841721
-6882881134
-4846848554
-5283751526"""
-
-
 def load_file():
     with open('../inputs/input11') as file:
         return file.read()
@@ -23,8 +5,6 @@ def load_file():
 
 def create_energy_map():
     data = load_file()
-    # data = test
-    # data = small_test
     return [[int(c) for c in line] for line in data.splitlines()]
 
 
