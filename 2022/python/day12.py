cedd7ad2f58049ed3f1d8d3b50bec366cec9163d
trox667 <trox667@googlemail.com>
2022-12-12 16:53:01 +0100

    day12: optimize

--- 2022/python/day12.py
+++ 2022/python/day12.py
@@ -1,75 +1,71 @@
 import sys
 from multiprocessing import Pool
+from heapq import heappop, heappush
 
 def dijkstra(start, end, heightmap):
 	assert(start[0] != end[0] or start[1] != end[1])
+
+	def to_hash(node):
+		return f'{node[0]}:{node[1]}'
 
 	heightmap[start[1]][start[0]] = ord('a')
 	heightmap[end[1]][end[0]] = ord('z')
 
 	queue = []
-	queue.append(start)
+	queue.append((0, start))
+
+	visited = []
+	distances = {to_hash(start): 0}
+	prev = {}
 
 	for y in range(len(heightmap)):
 		for x in range(len(heightmap[y])):
 			if x == start[0] and y == start[1]:
 				continue
 			else:
-				queue.append([x, y, sys.maxsize, None])
+				distances[to_hash([x, y])] = sys.maxsize
+				prev[to_hash([x, y])] = None
 
-	visited = []
-
-	def update_queue(queue):
-		return sorted(queue, key=lambda item: item[2])
-
-	paths = []
-	def path(node):
-		for v in visited:
-			if v[0] == node[0] and v[1] == node[1]:
-				prev = v[3]
-				if prev != None:
-					paths.append(prev)
-					path(prev)
-
-
-	while len(queue) > 0:
-		curr_position = queue.pop(0)
-		height_value = heightmap[curr_position[1]][curr_position[0]]
-		curr_value = curr_position[2]
+	while queue:
+		curr_value, curr = heappop(queue)
+		height_value = heightmap[curr[1]][curr[0]]
+		if curr in visited:
+			continue
+		visited.append(curr)
 
 		options = []
-		if curr_position[1]-1 >= 0:
-			x,y = curr_position[0], curr_position[1]-1
+		if curr[1]-1 >= 0:
+			x,y = curr[0], curr[1]-1
 			if heightmap[y][x]-height_value <= 1:
-				options.append([x, y, heightmap[y][x]-height_value])
-		if curr_position[1]+1 < len(heightmap):
-			x,y = curr_position[0], curr_position[1]+1
+				options.append([x, y])
+		if curr[1]+1 < len(heightmap):
+			x,y = curr[0], curr[1]+1
 			if heightmap[y][x]-height_value <= 1:
-				options.append([x, y, heightmap[y][x]-height_value])
-		if curr_position[0]-1 >= 0:
-			x,y = curr_position[0]-1, curr_position[1]
+				options.append([x, y])
+		if curr[0]-1 >= 0:
+			x,y = curr[0]-1, curr[1]
 			if heightmap[y][x]-height_value <= 1:
-				options.append([x, y, heightmap[y][x]-height_value])
-		if curr_position[0]+1 < len(heightmap[curr_position[1]]):
-			x,y = curr_position[0]+1, curr_position[1]
+				options.append([x, y])
+		if curr[0]+1 < len(heightmap[curr[1]]):
+			x,y = curr[0]+1, curr[1]
 			if heightmap[y][x]-height_value <= 1:
-				options.append([x, y, heightmap[y][x]-height_value])
+				options.append([x, y])
 
 		for option in options:
-			for item in queue:
-				if item[0] == option[0] and item[1] == option[1]:
-					new_effort = curr_value + heightmap[option[1]][option[0]]
-					if new_effort < item[2]:
-						item[2] = new_effort
-						item[3] = [curr_position[0], curr_position[1]]
-					break
+			new_effort = curr_value + heightmap[option[1]][option[0]]
+			if new_effort < distances[to_hash(option)]:
+				heappush(queue, (new_effort, option))
+				distances[to_hash(option)] = new_effort
+				prev[to_hash(option)] = curr
 
-		queue = update_queue(queue)
+	paths = []
+	curr = end
+	while curr != None:
+		paths.insert(0, curr)
+		curr = prev[to_hash(curr)] if to_hash(curr) in prev else None
+		if curr == start:
+			break
 
-		visited.append(curr_position)
-
-	path(end)
-	paths.reverse()
 	return len(paths)
 
 
@@ -77,9 +73,8 @@ with open('../inputs/input12') as f:
 # with open('./test12') as f:
 	heightmap = [[ord(x) for x in line.strip()] for line in f.readlines() if len(line) > 0]
 
-
-	start = [0, 0, 0, None]
-	end = [0, 0, sys.maxsize, None]
+	start = [0, 0]
+	end = [0, 0]
 	for y in range(len(heightmap)):
 		for x in range(len(heightmap[y])):
 			if heightmap[y][x] == 83:
@@ -88,13 +83,13 @@ with open('../inputs/input12') as f:
 			elif heightmap[y][x] == 69:
 				end[0] = x
 				end[1] = y
-	# print('Part 1:', dijkstra(start, end, heightmap))
+	print('Part 1:', dijkstra(start, end, heightmap))
 
 	starts = []
 	for y in range(len(heightmap)):
 		for x in range(len(heightmap[y])):
 			if heightmap[y][x] == ord('a'):
-				starts.append([x,y,0,None])
+				starts.append([x,y])
 	
-	p = Pool(12)
-	print('Part 2:', min([x for x in p.starmap(dijkstra, [(start, end, heightmap) for start in starts]) if x > 0]))
+	p = Pool(1)
+	print('Part 2:', min([x for x in p.starmap(dijkstra, [(start, end, heightmap) for start in starts]) if x > 1]))
