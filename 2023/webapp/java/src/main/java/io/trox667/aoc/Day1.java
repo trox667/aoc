32776d7448151823df97a14607970c0d38c56d9b
trox667 <trox667@googlemail.com>
2023-12-01 11:10:51 +0100

    2023: Java Day1

--- 2023/webapp/java/src/main/java/io/trox667/aoc/Day1.java
+++ 2023/webapp/java/src/main/java/io/trox667/aoc/Day1.java
@@ -1,19 +1,105 @@
 package io.trox667.aoc;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 public class Day1 extends Day {
     public Day1(Path path) {
         super(path);
     }
 
+    private String findDigits(String input) {
+        var result = input.replaceAll("[a-zA-Z]", "");
+        return result;
+    }
+
+    private boolean isDigit(String input) {
+        try {
+            Integer.parseInt(String.valueOf(input));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private String toDigit(String input) {
+        if (input.equals("one")) {
+            return "1";
+        } else if (input.equals("two")) {
+            return "2";
+        } else if (input.equals("three")) {
+            return "3";
+        } else if (input.equals("four")) {
+            return "4";
+        } else if (input.equals("five")) {
+            return "5";
+        } else if (input.equals("six")) {
+            return "6";
+        } else if (input.equals("seven")) {
+            return "7";
+        } else if (input.equals("eight")) {
+            return "8";
+        } else if (input.equals("nine")) {
+            return "9";
+        } else {
+            return null;
+        }
+    }
+
+    private String findNamedDigits(String input) {
+        var result = "";
+        for (var start = 0; start < input.length(); start++) {
+            for (var pos = start + 1; pos < start + 7 && pos < input.length()+1; pos++) {
+                var token = input.substring(start, pos);
+                if (isDigit(token)) {
+                    result += token;
+                    break;
+                } else if (toDigit(token) != null) {
+                    result += toDigit(token);
+                    break;
+                }
+            }
+        }
+
+
+        return result;
+    }
+
+    private int getFirstAndLastDigit(String input) {
+        List<String> digits = Arrays.asList(input.split(""));
+        return Integer.parseInt(digits.getFirst() + digits.getLast());
+    }
+
     @Override
     public <T> T part1() {
-        return (T)"Part 1";
+        var result = 0;
+        try {
+            var input = readInput();
+            for (var line : input) {
+                result += getFirstAndLastDigit(findDigits(line));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return (T) (Integer) result;
     }
 
     @Override
     public <T> T part2() {
-        return (T)"Part 2";
+        var result = 0;
+        try {
+            var input = readInput();
+            for (var line : input) {
+                var tmp = getFirstAndLastDigit(findNamedDigits(line));
+                result += tmp;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return (T) (Integer) result;
     }
 }
--- 2023/webapp/java/src/test/java/TestDay1.java
+++ 2023/webapp/java/src/test/java/TestDay1.java
@@ -1,2 +1,20 @@
+import io.trox667.aoc.Day1;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestDay1 {
+    @Test
+    public void testPart1() {
+        assertEquals((Integer)142, new Day1(Paths.get("../../input/sample01")).part1());
+        assertEquals((Integer)54338, new Day1(Paths.get("../../input/input01")).part1());
+    }
+
+    @Test
+    public void testPart2() {
+        assertEquals((Integer)281, new Day1(Paths.get("../../input/sample01_2")).part2());
+        assertEquals((Integer)53389, new Day1(Paths.get("../../input/input01")).part2());
+    }
 }
