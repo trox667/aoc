359135d99006b3e939166fd45d03eaf4cfbbf94f
trox667 <trox667@googlemail.com>
2023-12-06 08:08:46 +0100

    2023: Java Day6
    ugly

--- 2023/webapp/java/src/main/java/io/trox667/aoc/Day6.java
+++ 2023/webapp/java/src/main/java/io/trox667/aoc/Day6.java
@@ -1,19 +1,76 @@
 package io.trox667.aoc;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.LongStream;
 
 public class Day6 extends Day {
+
+    private static class RaceDocument {
+        private List<Long> times;
+        private List<Long> distances;
+
+        public RaceDocument() {
+            times = new ArrayList<>();
+            distances = new ArrayList<>();
+        }
+
+        public static RaceDocument fromStrings(List<String> lines) {
+            assert lines.size() >= 2;
+            var raceDocument = new RaceDocument();
+            var times = Arrays.stream(lines.get(0).split(": ")[1].trim().split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).toList();
+            var distances = Arrays.stream(lines.get(1).split(": ")[1].trim().split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).toList();
+            raceDocument.times = times;
+            raceDocument.distances = distances;
+            return raceDocument;
+        }
+
+        public static RaceDocument fromStringsOneRace(List<String> lines) {
+            assert lines.size() >= 2;
+            var raceDocument = new RaceDocument();
+            var time = Long.parseLong(lines.get(0).split(": ")[1].trim().replaceAll(" ", ""));
+            var distance = Long.parseLong(lines.get(1).split(": ")[1].trim().replaceAll(" ", ""));
+            raceDocument.times.add(time);
+            raceDocument.distances.add(distance);
+            return raceDocument;
+        }
+
+        public long getMultipliedNumberOfWaysToBeatTheRecords() {
+            assert times.size() == distances.size();
+            var result = 1;
+            for (var i = 0; i < times.size(); i++) {
+                var time = times.get(i);
+                var distance = distances.get(i);
+                result *= LongStream.range(1, time).map(speed -> (time - speed) * speed).filter(pd -> pd > distance).count();
+            }
+            return result;
+        }
+    }
+
     public Day6(Path path) {
         super(path);
     }
 
     @Override
     public Object part1() {
-        return null;
+        try {
+            var raceDocument = RaceDocument.fromStrings(readInput());
+            return raceDocument.getMultipliedNumberOfWaysToBeatTheRecords();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Object part2() {
-        return null;
+        try {
+            var raceDocument = RaceDocument.fromStringsOneRace(readInput());
+            return raceDocument.getMultipliedNumberOfWaysToBeatTheRecords();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- 2023/webapp/java/src/test/java/TestDay6.java
+++ 2023/webapp/java/src/test/java/TestDay6.java
@@ -9,13 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestDay6 {
     @Test
     public void part1() {
-        Assertions.assertEquals(0, new Day6(Paths.get("../../input/sample06")).part1());
-        assertEquals(0, new Day6(Paths.get("../../input/input06")).part1());
+        assertEquals((long) 288, new Day6(Paths.get("../../input/sample06")).part1());
+        assertEquals((long) 131376, new Day6(Paths.get("../../input/input06")).part1());
     }
 
     @Test
     public void part2() {
-        assertEquals(0, new Day6(Paths.get("../../input/sample06")).part2());
-        assertEquals(0, new Day6(Paths.get("../../input/input06")).part2());
+        assertEquals((long) 71503, new Day6(Paths.get("../../input/sample06")).part2());
+        assertEquals((long) 34123437, new Day6(Paths.get("../../input/input06")).part2());
     }
 }
