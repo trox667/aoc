9e8d0d8030210b0b505ac78bdbbc47ef6da8b962
trox667 <trox667@googlemail.com>
2023-12-15 22:58:00 +0100

    2023: Java Day15
    some cleanup

--- 2023/webapp/java/src/main/java/io/trox667/aoc/Day15.java
+++ 2023/webapp/java/src/main/java/io/trox667/aoc/Day15.java
@@ -9,12 +9,10 @@ import java.util.ArrayList;
 
 public class Day15 extends Day {
     private static class HASHAlgorithm {
-
         public static long getHash(String input) {
             var hash = 0L;
             for (var c : input.toCharArray()) {
-                var code = (int) c;
-                hash += code;
+                hash += (int) c;
                 hash *= 17;
                 hash %= 256;
             }
@@ -32,13 +30,10 @@ public class Day15 extends Day {
         }
     }
 
-    private record Step(String label, String operation, int focalLength) {
-        public long getHash() {
-            return HASHAlgorithm.getHash(this.label);
-        }
+    private record Lens(String label, int focalLength) {
     }
 
-    private record Lens(String label, int focalLength) {};
+    ;
 
     private static class Box {
         private HashMap<String, Integer> labelsToLenses;
@@ -49,22 +44,21 @@ public class Day15 extends Day {
             this.lenses = new ArrayList<Lens>();
         }
 
-        public Lens remove(String label) {
+        public void remove(String label) {
             if (labelsToLenses.containsKey(label)) {
                 int index = labelsToLenses.get(label);
                 labelsToLenses.remove(label);
-                var oldLense = lenses.remove(index);
+                lenses.remove(index);
                 for (var i = index; i < lenses.size(); i++) {
                     labelsToLenses.put(lenses.get(i).label(), i);
                 }
             }
-            return null;
         }
 
         public void add(Lens lens) {
             if (labelsToLenses.containsKey(lens.label())) {
                 int index = labelsToLenses.get(lens.label());
-                var oldLens = lenses.set(index, lens);
+                lenses.set(index, lens);
             } else {
                 labelsToLenses.put(lens.label(), lenses.size());
                 lenses.add(lens);
@@ -82,19 +76,17 @@ public class Day15 extends Day {
         public static Facility fromStrings(List<String> lines) {
             var facility = new Facility();
             for (var line : lines) {
-                var steps = line.split(",");
-                for (var step : steps) {
+                for (var step : line.split(",")) {
                     var tokens = step.split("=|-");
                     var label = tokens[0];
                     var focalLength = tokens.length > 1 ? Integer.parseInt(tokens[1]) : 0;
-                    var operation = step.contains("=") ? "=" : "-";
                     var hash = HASHAlgorithm.getHash(label);
                     if (!facility.boxes.containsKey(hash)) {
                         facility.boxes.put(hash, new Box());
                     }
-                    if (operation == "=") {
+                    if (step.contains("=")) {
                         facility.boxes.get(hash).add(new Lens(label, focalLength));
-                    } else {
+                    } else if (step.contains("-")) {
                         facility.boxes.get(hash).remove(label);
                     }
                 }
