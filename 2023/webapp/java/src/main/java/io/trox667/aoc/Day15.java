e9fa687328cc18b1ea9def93c1b66cd19845f39d
trox667 <trox667@googlemail.com>
2023-12-15 14:42:22 +0100

    2023: Java Day15
    ugly

--- 2023/webapp/java/src/main/java/io/trox667/aoc/Day15.java
+++ 2023/webapp/java/src/main/java/io/trox667/aoc/Day15.java
@@ -3,7 +3,9 @@ package io.trox667.aoc;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Day15 extends Day {
     private static class HASHAlgorithm {
@@ -22,14 +24,99 @@ public class Day15 extends Day {
 
     private static class InitializationSequence {
         public static long fromStrings(List<String> lines) {
-            var initializationSequence = new InitializationSequence();
             var result = 0L;
             for (var line : lines) {
                 result += Arrays.stream(line.split(",")).map(HASHAlgorithm::getHash).reduce(0L, Long::sum);
             }
-
-
             return result;
+        }
+    }
+
+    private record Step(String label, String operation, int focalLength) {
+        public long getHash() {
+            return HASHAlgorithm.getHash(this.label);
+        }
+    }
+
+    private record Lens(String label, int focalLength) {};
+
+    private static class Box {
+        private HashMap<String, Integer> labelsToLenses;
+        private List<Lens> lenses;
+
+        public Box() {
+            this.labelsToLenses = new HashMap<>();
+            this.lenses = new ArrayList<Lens>();
+        }
+
+        public Lens remove(String label) {
+            if (labelsToLenses.containsKey(label)) {
+                int index = labelsToLenses.get(label);
+                labelsToLenses.remove(label);
+                var oldLense = lenses.remove(index);
+                for (var i = index; i < lenses.size(); i++) {
+                    labelsToLenses.put(lenses.get(i).label(), i);
+                }
+            }
+            return null;
+        }
+
+        public void add(Lens lens) {
+            if (labelsToLenses.containsKey(lens.label())) {
+                int index = labelsToLenses.get(lens.label());
+                var oldLens = lenses.set(index, lens);
+            } else {
+                labelsToLenses.put(lens.label(), lenses.size());
+                lenses.add(lens);
+            }
+        }
+    }
+
+    private static class Facility {
+        private HashMap<Long, Box> boxes;
+
+        public Facility() {
+            this.boxes = new HashMap<>();
+        }
+
+        public static Facility fromStrings(List<String> lines) {
+            var facility = new Facility();
+            for (var line : lines) {
+                var steps = line.split(",");
+                for (var step : steps) {
+                    var tokens = step.split("=|-");
+                    var label = tokens[0];
+                    var focalLength = tokens.length > 1 ? Integer.parseInt(tokens[1]) : 0;
+                    var operation = step.contains("=") ? "=" : "-";
+                    var hash = HASHAlgorithm.getHash(label);
+                    if (!facility.boxes.containsKey(hash)) {
+                        facility.boxes.put(hash, new Box());
+                    }
+                    if (operation == "=") {
+                        facility.boxes.get(hash).add(new Lens(label, focalLength));
+                    } else {
+                        facility.boxes.get(hash).remove(label);
+                    }
+                }
+            }
+            return facility;
+        }
+
+        public long getFocusingPower() {
+            var overallPower = 0L;
+            for (var i = 0L; i < 256L; i++) {
+                if (this.boxes.containsKey(i)) {
+                    var box = this.boxes.get(i);
+                    if (box.lenses.size() == 0) {
+                        continue;
+                    }
+                    var boxIndex = i + 1L;
+                    for (var j = 0; j < box.lenses.size(); j++) {
+                        overallPower += boxIndex * (j + 1) * box.lenses.get(j).focalLength;
+                    }
+                }
+            }
+            return overallPower;
         }
     }
 
@@ -48,7 +135,11 @@ public class Day15 extends Day {
 
     @Override
     public Object part2() {
-        
-        return null;
+        try {
+            var facility = Facility.fromStrings(this.readInput());
+            return facility.getFocusingPower();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- 2023/webapp/java/src/test/java/TestDay15.java
+++ 2023/webapp/java/src/test/java/TestDay15.java
@@ -9,12 +9,12 @@ public class TestDay15 {
     @Test
     public void part1() {
         assertEquals( 1320L, new Day15(Paths.get("../../input/sample15")).part1());
-        assertEquals( 0L, new Day15(Paths.get("../../input/input15")).part1());
+        assertEquals( 511416L, new Day15(Paths.get("../../input/input15")).part1());
     }
 
     @Test
     public void part2() {
-        assertEquals(0L, new Day15(Paths.get("../../input/sample15")).part2());
-//        assertEquals(0L, new Day15(Paths.get("../../input/input15")).part2());
+        assertEquals(145L, new Day15(Paths.get("../../input/sample15")).part2());
+        assertEquals(290779L, new Day15(Paths.get("../../input/input15")).part2());
     }
 }
